0a19ffd23ef40673d93ed50ef5d745295a5eca7e
musphilo <fm_bouguesri@esi.dz>
2018-08-12 00:10:09 +0100

    added package-lock and tests file that were forgotten

---
+++ utils/utils.test.js
@@ -0,0 +1,40 @@
+/**************** Node and 3rd party Modules ****************/
+const expect = require('expect');
+
+/******************** personnel Modules *********************/
+const util = require('./utils');
+
+it("should add two numbers",() =>{
+    var res = util.add(15, 25);
+    expect(res).toBeA('number').toBe(40);
+        
+});
+
+it("should square a number",() =>{
+    var res = util.square(5);
+    expect(res).toBeA('number').toBe(25);
+        
+});
+
+it('should set firstName and lastName', () => {
+    var user = {location: 'Philadelphia', age: 25};
+    var res = util.setName(user, 'Andrew Mead');
+  
+    expect(res).toInclude({
+      firstName: 'Andrew',
+      lastName: 'Mead'
+    });
+});
+
+it('should expect some values', () => {
+  expect(12).toNotBe(13);
+  expect({name: 'andrew'}).toNotEqual({name: 'Andrew'});
+  expect([2,3,4]).toExclude(1);
+  expect({
+    name: 'Andrew',
+    age: 25,
+    location: 'Philadelphia'
+  }).toExclude({
+    age: 23
+  })
+});
