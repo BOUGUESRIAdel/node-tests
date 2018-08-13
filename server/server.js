a15c6dfd16fbe7ae5035bb4951cbc82169c3a283
musphilo <fm_bouguesri@esi.dz>
2018-08-13 07:52:15 +0100

    added supertest api : - played with some of it properties (status code, custom function, response object) added /users route to test array response - tried testing error case with custom error object and not Error

--- server/server.js
+++ server/server.js
@@ -9,7 +9,24 @@ const port = process.env.PORT || 3000;
 
 /**************************** Code ****************************/
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.status(404).send({
+        error : 'Page not found',
+        name : 'Todo v1.0'
+    });
+    
+});
+
+app.get('/users', (req, res) => {
+    res.send([{
+        name : 'Adel',
+        age : '22'
+    },{
+        name : 'musphilo',
+        age : '23'
+    },{
+        name : 'bocka',
+        age : '90'
+    }]);
     
 });
 
@@ -17,3 +34,5 @@ app.listen(port, () => {
     console.log(`server running on port : ${port}`);
     
 });
+
+module.exports.app = app;
