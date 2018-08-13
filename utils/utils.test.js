8dd3124ad81c023fae9c0e73014ce9c14eb88aa1
musphilo <fm_bouguesri@esi.dz>
2018-08-13 08:05:33 +0100

    organized tests with describe

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -4,34 +4,40 @@ const expect = require('expect');
 /******************** personnel Modules *********************/
 const util = require('./utils');
 
-it("should add two numbers",() =>{
-    var res = util.add(15, 25);
-    expect(res).toBeA('number').toBe(40);
-        
-});
+describe('Utils Async', () => {
 
-it("should async add two numbers",() =>{
-  return util.addAsync(15, 25).then((sum) => {
-    expect(sum).toBeA('number').toBe(40);
+  it("should async add two numbers",() =>{
+    return util.addAsync(15, 25).then((sum) => {
+      expect(sum).toBeA('number').toBe(40);
+    });
+        
   });
-      
-});
 
-it("should square a number",() =>{
-    var res = util.square(5);
-    expect(res).toBeA('number').toBe(25);
-        
-});
-
-it("should async square a number",(done) =>{
+  it("should async square a number",(done) =>{
     util.squareAsync(5, (square) => {
       expect(square).toBeA('number').toBe(25);
       done();
     });
       
+  });
+
 });
 
-it('should set firstName and lastName', () => {
+describe('Utils Sync', () => {
+  
+  it("should add two numbers",() =>{
+    var res = util.add(15, 25);
+    expect(res).toBeA('number').toBe(40);
+          
+  });
+
+  it("should square a number",() =>{
+    var res = util.square(5);
+    expect(res).toBeA('number').toBe(25);
+        
+  });
+
+  it('should set firstName and lastName', () => {
     var user = {location: 'Philadelphia', age: 25};
     var res = util.setName(user, 'Andrew Mead');
   
@@ -39,17 +45,30 @@ it('should set firstName and lastName', () => {
       firstName: 'Andrew',
       lastName: 'Mead'
     });
+  });
 });
 
-it('should expect some values', () => {
-  expect(12).toNotBe(13);
-  expect({name: 'andrew'}).toNotEqual({name: 'Andrew'});
-  expect([2,3,4]).toExclude(1);
-  expect({
-    name: 'Andrew',
-    age: 25,
-    location: 'Philadelphia'
-  }).toExclude({
-    age: 23
-  })
+describe('Playing with expect', () => {
+  it('should expect some values', () => {
+    expect(12).toNotBe(13);
+    expect({name: 'andrew'}).toNotEqual({name: 'Andrew'});
+    expect([2,3,4]).toExclude(1);
+    expect({
+      name: 'Andrew',
+      age: 25,
+      location: 'Philadelphia'
+    }).toExclude({
+      age: 23
+    })
+  });
+  
 });
+
+
+
+
+
+
+
+
+
